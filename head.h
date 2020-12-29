b045ea57dd417ba953258cd37035af21b02b8551
浩 李 <penghao.lee@outlook.com>
2020-12-29 09:44:15 +0800

    Initial commit

---
+++ head.h
@@ -0,0 +1,14 @@
+//
+// Created by charol on 2020/12/29.
+//
+
+#ifndef TURING_MACHINE_HEAD_H
+#define TURING_MACHINE_HEAD_H
+
+typedef struct {
+
+    int i_condition;
+
+};
+
+#endif //TURING_MACHINE_HEAD_H
